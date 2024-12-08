1fd8c7442a2e4de7233d8067cc96d32cdfcda21e
om-ghante <omghante.official@gmail.com>
2024-12-09 00:12:55 +0530

    Start v2.0

--- synfuse-client-v2.0/src/App.jsx
+++ synfuse-client-v2.0/src/App.jsx
@@ -1,14 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+import Navbar from './components/navbar.component/index'
+
+import HeroSection from './components/home.page/index'
+import AboutUs from './components/aboutus.page/index'
+import SynfuseTeam from './components/team.page/index'
+import ContanctUs from './components/contactus.page/index'
+
+import PageNotFound from './components/pagenotfound.page/index'
+
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <h1>Start</h1>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Navbar />}>
+            <Route index path='/home' element={<HeroSection />}/>
+            <Route path='/aboutus' element={<AboutUs />}/>
+            <Route path='/team' element={<SynfuseTeam />}/>
+            <Route path='/contactus' element={<ContanctUs />}/>
+          </Route>
+          <Route path='*' element={<PageNotFound />}/>
+        </Routes>
+      </Router>
     </>
   )
 }
---
+++ synfuse-client-v2.0/src/components/navbar.component/index.jsx
@@ -0,0 +1,152 @@
+import React from 'react'
+
+import { Outlet } from 'react-router-dom'
+
+import FooterComponent from '../footer.component';
+
+const NavbarComponent = () => {
+  const [openNav, setOpenNav] = React.useState(false);
+ 
+  React.useEffect(() => {
+    window.addEventListener(
+      "resize",
+      () => window.innerWidth >= 960 && setOpenNav(false),
+    );
+  }, []);
+ 
+  const navList = (
+    <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
+      <p
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Pages
+        </a>
+      </p>
+      <p
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Account
+        </a>
+      </p>
+      <p
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Blocks
+        </a>
+      </p>
+      <p
+        as="li"
+        variant="small"
+        color="blue-gray"
+        className="p-1 font-normal"
+      >
+        <a href="#" className="flex items-center">
+          Docs
+        </a>
+      </p>
+    </ul>
+  );
+ 
+  return (
+    <>
+      <div className="-m-6 max-h-[768px] w-[calc(100%+48px)] overflow-scroll">
+        <navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4">
+          <div className="flex items-center justify-between text-blue-gray-900">
+            <p
+              as="a"
+              href="#"
+              className="mr-4 cursor-pointer py-1.5 font-medium"
+            >
+              Material Tailwind
+            </p>
+            <div className="flex items-center gap-4">
+              <div className="mr-4 hidden lg:block">{navList}</div>
+              <div className="flex items-center gap-x-1">
+                <button
+                  variant="text"
+                  size="sm"
+                  className="hidden lg:inline-block"
+                >
+                  <span>Log In</span>
+                </button>
+                <button
+                  variant="gradient"
+                  size="sm"
+                  className="hidden lg:inline-block"
+                >
+                  <span>Sign in</span>
+                </button>
+              </div>
+              <button
+                variant="text"
+                className="ml-auto h-6 w-6 text-inherit hover:bg-transparent focus:bg-transparent active:bg-transparent lg:hidden"
+                ripple={false}
+                onClick={() => setOpenNav(!openNav)}
+              >
+                {openNav ? (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    className="h-6 w-6"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M6 18L18 6M6 6l12 12"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    className="h-6 w-6"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth={2}
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M4 6h16M4 12h16M4 18h16"
+                    />
+                  </svg>
+                )}
+              </button>
+            </div>
+          </div>
+          <navbar className="" open={openNav}>
+            {navList}
+            <div className="flex items-center gap-x-1">
+              <button fullWidth variant="text" size="sm" className='color-red'>
+                Log In
+              </button>
+              <button fullWidth variant="gradient" size="sm" className="">
+                Sign in
+              </button>
+            </div>
+          </navbar>
+        </navbar>
+        <div className="mx-auto max-w-screen-md py-12">
+            <Outlet />
+        </div>
+      </div>
+      <FooterComponent />
+    </>
+  );
+}
+
+export default NavbarComponent
